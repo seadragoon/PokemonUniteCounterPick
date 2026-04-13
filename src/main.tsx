83925e7455b01e548114ebc4673707eed5f4efdb
seadragoon <seadragoon18@gmail.com>
2026-04-13 15:16:26 +0900

    google analytics追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import './index.css'
 import App from './App.tsx'
+
+const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+if (GA_MEASUREMENT_ID) {
+  ReactGA.initialize(GA_MEASUREMENT_ID);
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
